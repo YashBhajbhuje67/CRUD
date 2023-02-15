162d1d4ba05566a87deb785cbc217af57f2cf663
Yash Bhajbhuje <yashbhajbhuje67@gmail.com>
2023-02-15 19:04:43 +0530

    Update index.js

--- crud/src/index.js
+++ crud/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import axios from 'axios';
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = 'https://crud-sable.vercel.app';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
